903eaaf5e242191785c079a18726ed4710db0520
Donmark2k <chukwuemekavp@gmail.com>
2023-10-15 18:50:15 +0100

    Run npm i bcrypt

--- contacts-backend/controllers/userController.js
+++ contacts-backend/controllers/userController.js
@@ -1,9 +1,38 @@
-// const asyncHandler = require("express-async-handler");
-// const Contact = require ("../models/contactModel")
-//@desc Get all contacts
-//@route GET /api/contacts
+const asyncHandler = require("express-async-handler");
+const User = require ("../models/userModel");
+//@desc Register a user
+//@route POST /api/users/register
 //@access public
-const getContacts = asyncHandler(async(req, res) => {
-    const contacts = await Contact.find()
-    res.status(200).json(contacts);
+const registerUser = asyncHandler(async(req, res) => {
+    const {username, email, password} = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are required");
+    }
+
+    const userAvailable = await User.findOne({ email});
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+
+    const user = await User.create({
+        username, email,password,
+    })
+    res.status(201).json(user);})
+
+//@desc login user
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async(req, res) => {
+    res.json({ message: "login user"});
 })
+
+//@desc current user info
+//@route POST /api/users/login
+//@access private
+const currentUser = asyncHandler(async(req, res) => {
+    res.json({ message: "current user info"});
+})
+
+module.exports = {registerUser, loginUser, currentUser};
--- contacts-backend/routes/userRoutes.js
+++ contacts-backend/routes/userRoutes.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const {registerUser, loginUser, currentUser} = require("../controllers/userController");
 
-router.post("/register", (req, res) => {
-    res.json({ message: "Register the user"});
-});
+router.post("/register", registerUser);
 
-router.post("/login", (req, res) => {
-    res.json({ message: "login user"});
-});
+router.post("/login", loginUser);
 
-router.get("/current", (req, res) => {
-    res.json({ message: "current user information"});
-});
+router.get("/current", currentUser);
 
 module.exports = router;
